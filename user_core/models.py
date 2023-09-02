be33cd833ae46a5186a13602f884ce2e41932707
bugratemirci <bugrahanetemircii@gmail.com>
2023-09-02 15:12:56 +0300

    add photo and album model

--- user_core/models.py
+++ user_core/models.py
@@ -15,7 +15,9 @@ class User(BaseModel):
     website = models.CharField(max_length=100)
     company = models.JSONField()
     address = models.JSONField()
-    photo_path = models.CharField(max_length=200, null=True, blank=True)
+    root_path = models.CharField(max_length=200)
+    profile_photo_path = models.CharField(
+        max_length=200, null=True, blank=True)
 
 
 class Post(BaseModel):
@@ -35,3 +37,16 @@ class Todo(BaseModel):
     title = models.TextField()
     completed = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=False)
+
+
+class Photo(BaseModel):
+    title = models.TextField()
+    url = models.TextField()
+    thumbnail_url = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Album(BaseModel):
+    title = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photos = models.ManyToManyField(Photo, null=True, blank=True)
