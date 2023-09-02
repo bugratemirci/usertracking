58f099778b38615577ab8630d4e553eb9469a7c9
bugratemirci <bugrahanetemircii@gmail.com>
2023-09-02 15:13:36 +0300

    add photo and album viewsets to urls

--- user_core/urls.py
+++ user_core/urls.py
@@ -7,6 +7,8 @@ router.register(r'users', views.UserViewset)
 router.register(r'posts', views.PostViewset)
 router.register(r'comments', views.CommentViewset)
 router.register(r'todos', views.TodoViewset)
+router.register(r'photos', views.PhotoViewset)
+router.register(r'albums', views.AlbumViewset)
 
 
 urlpatterns = [
