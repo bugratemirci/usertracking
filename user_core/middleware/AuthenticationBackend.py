540921a0dce7165e16b555598eb54e83ab127a62
bugratemirci <bugrahanetemircii@gmail.com>
2023-09-09 00:40:36 +0300

    add null checks and raise exceptions

--- user_core/middleware/AuthenticationBackend.py
+++ user_core/middleware/AuthenticationBackend.py
@@ -2,22 +2,26 @@ from django.contrib.auth.backends import BaseBackend
 from ..models import User
 from rest_framework.authentication import get_authorization_header
 from ..utils.Authentication import decode_access_token
+from ..exception.BadRequestException import BadRequestException
 
 
 class AuthenticationBackend(BaseBackend):
     def authenticate(self, request):
-        auth = get_authorization_header(request).split()
+        try:
+            auth = get_authorization_header(request).split()
+            if (auth and len(auth) == 2):
+                if auth[0].decode() == 'Bearer':
 
-        if (auth and len(auth) == 2):
-            if auth[0].decode() == 'Bearer':
-                user_id = decode_access_token(auth[1].decode())
-                
-                user = User.objects.get(id=user_id)
-                return [user, None]
-        return None
+                    user_id = decode_access_token(auth[1].decode())
+                    print(user_id)
+
+                    user = User.objects.get(id=user_id)
+                    return [user, None]
+        except Exception as e:
+            raise BadRequestException("User id not found in bearer token.")
 
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
-            return None
+            raise BadRequestException("User does not exists.")
--- user_core/views/UserViewset.py
+++ user_core/views/UserViewset.py
@@ -11,6 +11,8 @@ from django.db.models import Q
 from ..models import User
 from drf_yasg.utils import swagger_auto_schema, no_body, status
 from drf_yasg import openapi
+from ..exception.BadRequestException import BadRequestException
+from ..constants.Response import SuccessResponse
 
 
 class UserViewset(ModelViewSet):
@@ -39,8 +41,10 @@ class UserViewset(ModelViewSet):
     )
     @action(methods=['PUT'], detail=False, url_path='uploadprofilephoto')
     def upload_profile_photo(self, request):
-        user_id = request.query_params.get('user_id')
-        file = request.data['file']
+        user_id = request.query_params.get('user_id', -1)
+        file = request.data.get('file', -1)
+        if user_id == -1 or file == -1:
+            raise BadRequestException("File or user id can't be null.")
         file_path = FileUtils(file, user_id).moveProfilePhotoToUserFolder()
         user = User.objects.get(id=user_id)
         user.profile_photo_path = file_path
@@ -59,13 +63,17 @@ class UserViewset(ModelViewSet):
     )
     @action(methods=['POST'], detail=False, url_path='login', permission_classes=[AllowAny])
     def login(self, request):
-        email = request.data['username']
-        password = request.data['password']
-        user = User.objects.get(username=email)
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+        if len(username) == 0 or len(password) == 0:
+            raise BadRequestException("Username or password can't be empty.")
+
+        user = User.objects.get(username=username)
         if (user.check_password(password)):
             token = create_access_token(user.id)
-            return Response(token)
-        return Response({'error': 'Invalid email or password'})
+            return Response(SuccessResponse(token).__dict__)
+
+        raise BadRequestException('Invalid username or password')
 
     @swagger_auto_schema(
         request_body=UserSerializerForRegister,
@@ -77,10 +85,10 @@ class UserViewset(ModelViewSet):
         user = FolderUtils(data).createUserFolder()
         hashed_password = make_password(user['password'])
         user['password'] = hashed_password
-        user_serializer = self.get_serializer(data=user)
+        user_serializer = UserSerializerForRegister(data=user)
         user_serializer.is_valid(raise_exception=True)
         user_serializer.save()
-        return Response(user_serializer.data)
+        return Response(SuccessResponse("User created successfully.").__dict__)
 
     @swagger_auto_schema(
         manual_parameters=[
