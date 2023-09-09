fa5ce015852bf1fbb9c429e5df3ba24ca55a35a0
bugratemirci <bugrahanetemircii@gmail.com>
2023-09-09 14:47:45 +0300

    add authenticate allow swagger, redoc, login and register paths

--- user_core/middleware/AuthenticationBackend.py
+++ user_core/middleware/AuthenticationBackend.py
@@ -7,18 +7,25 @@ from ..exception.BadRequestException import BadRequestException
 
 class AuthenticationBackend(BaseBackend):
     def authenticate(self, request):
+        base_url = 'http://localhost:8000/'
+        allowed_uris = [base_url + 'users/login/',
+                        base_url + 'users/register/',
+                        base_url + 'swagger/',
+                        base_url + 'swagger/?format=openapi',
+                        base_url + 'redoc/',
+                        base_url + 'redoc/?format=openapi']
         try:
+            if len(get_authorization_header(request).split()) == 0 and request.build_absolute_uri() not in allowed_uris:
+                raise BadRequestException("Authorization not found in request")
             auth = get_authorization_header(request).split()
             if (auth and len(auth) == 2):
                 if auth[0].decode() == 'Bearer':
 
                     user_id = decode_access_token(auth[1].decode())
-                    print(user_id)
-
                     user = User.objects.get(id=user_id)
                     return [user, None]
         except Exception as e:
-            raise BadRequestException("User id not found in bearer token.")
+            raise BadRequestException(e.detail)
 
     def get_user(self, user_id):
         try:
